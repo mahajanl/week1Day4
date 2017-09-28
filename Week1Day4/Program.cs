feb9ce90c0d33789ede8d21f10debeae54c7cb8b
mahajanl <lakshamima@gmail.com>
2017-09-28 13:24:03 -0400

    exceptions and string with a little indexing

--- Week1Day4/Program.cs
+++ Week1Day4/Program.cs
@@ -10,6 +10,19 @@ namespace Week1Day4
     {
         static void Main(string[] args)
         {
+            // string is a sequence oc hcaracters inside quotes and can be punct, numb, or letters
+            // much like an array 
+
+            Console.WriteLine("This is a tab \t.");
+            Console.WriteLine("This is a \"quote\".");
+            Console.WriteLine("This is a backslash \\");
+            Console.WriteLine("This is a \n new line.");
+            Console.WriteLine("This is a bell \a");
+
+            string firstName = "Lakshami";
+            string dogName = "Cleo";
+
+            Console.WriteLine("My name is {0} and my dog is {1}.", firstName, dogName);
         }
     }
 }
